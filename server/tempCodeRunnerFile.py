f9bbf8ffff96ca43542907e40ddc2a58d41c3bfd
SanchitaJindal <sanchitajindal04@gmail.com>
2024-08-14 20:43:00 +0530

    Flask Server

---
+++ server/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+   print(classify_image(get_b64_test_image_for_virat(), None))
+    print(classify_image(None, "server/test_images/federer1.jpg"))
+    print(classify_image(None, "server/test_images/federer2.jpg"))
+    print(classify_image(None, "server/test_images/virat1.jpg"))
